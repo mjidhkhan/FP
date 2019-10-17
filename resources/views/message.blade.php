1e27d6132e801d3d813183fc24a3f9208536f179
MH Khan <majidhkhan@outlook.com>
2019-10-17 23:00:36 +0100

    session message updated

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -7,4 +7,12 @@
             </button>
             </div>
         @endif
+        @if(session()->get('message'))
+            <div class="alert alert-success">
+           <strong>{{ session()->get('message') }}</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+        @endif
 </div>
